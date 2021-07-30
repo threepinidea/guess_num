39568f367d87beadb5ef3addb1c6b12d1fab8d35
unknown <threepinidea@gmaill.com>
2021-07-30 21:46:27 +0800

    add count

--- import-random.py
+++ import-random.py
@@ -4,7 +4,9 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1
     num = int(input('請猜數字: '))
     if num == r:
         print('猜中了!')
@@ -13,3 +15,4 @@ while True:
         print('提示: 比答案大')
     elif num < r:
         print('提示: 比答案小')
+    print('這是你猜的第', count, '次')
